70d8cc350fb1c8996c6f6455a905e0988de319ec
Xafree <emmanueleberst@hotmail.com>
2022-03-02 23:56:14 +0100

    first test pass

--- tests/test.ts
+++ tests/test.ts
@@ -1,8 +1,6 @@
 import { Restaurant } from '../src/Classes/Restaurant'; // this will be your custom import
 import { expect } from 'chai';
 import { MaitreHotel } from '../src/Classes/MaitreHotel';
-import assertArrays from 'chai-arrays';
-
 
 describe('DebutService', () => {
   it('ÉTANT DONNE un restaurant ayant 3 tables, QUAND le service commence, ALORS elles sont toutes affectées au Maître d\'Hôtel',
@@ -12,6 +10,6 @@ describe('DebutService', () => {
 
       restaurant.demarrerService(maitreHotel);
 
-      expect(restaurant.tables).to.be.equalTo(maitreHotel.tables);
+      expect(restaurant.tables).to.have.all.members(maitreHotel.tables);
     })
 })
